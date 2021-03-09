374004195090a6126d7b4bb9128062c25da089ef
Ruben Astudillo <ruben.astud@gmail.com>
2021-03-09 14:20:46 -0300

    Rpn based

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 var stack = [];
-var cur_value = zero();
 var op_stack = [];
+var cur_value = zero();
 
 const def_op = {
-    sum: (a,b) => a + b,
-    minus: (a,b) => a - b,
-    multiplication: (a,b) => a * b,
-    division: (a,b) => a / b
+    sum: {kind: "op", name: "sum", fun: (a,b) => a + b, priority: 1},
+    minus: {kind: "op", name: "minus", fun: (a,b) => a - b, priority: 1},
+    mult: {kind: "op", name: "mult", fun: (a,b) => a * b, priority: 2},
+    div: {kind: "op", name: "div", fun: (a,b) => a / b, priority: 2}
 };
 
 function num_constructor(a) {
-    return {value: a, order: 0, setting_decimal: false};
+    return {kind: "num", value: a, order: 0, setting_decimal: false};
 }
 function zero() { return num_constructor(0); }
 
 function setDecimal() {
     cur_value.setting_decimal = true;
     cur_value.order = -1;
-    showValue();
+    showValue(cur_value.value);
 }
 
 function addDigit(digit) {
@@ -40,7 +40,7 @@ function addDigit(digit) {
 
 function negate() {
     cur_value.value = (-1) * cur_value.value;
-    showValue();
+    showValue(cur_value.value);
 }
 
 function showValue(val) {
@@ -48,26 +48,70 @@ function showValue(val) {
     panel.textContent = val.toString();
 }
 
+// https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+function pushNumber(num) {
+    stack.push(num);
+}
+
+function pushOperator(thisop) {
+    if (op_stack.length >= 1) {
+        const prevop = op_stack[0];
+        if (thisop.priority > prevop.priority) {
+            op_stack.unshift(thisop);
+        } else {
+            op_stack.shift();
+            op_stack.unshift(thisop);
+            stack.push(prevop);
+        }
+    } else {
+        op_stack.unshift(thisop);
+    }
+}
+
+function optimisticEval() {
+    const len = stack.length;
+    if (len >= 3 && stack[len - 1].kind == "op"
+        && stack[len - 2].kind == "num"
+        && stack[len - 3].kind == "num") {
+        const op = stack.pop().fun;
+        const b = stack.pop().value;
+        const a = stack.pop().value;
+        const res = op(a,b);
+        pushNumber(num_constructor(res));
+        return [true, res];
+    } else {
+        return [false];
+    }
+}
+
 function setOperand(op) {
-    stack.push(cur_value.value);
-    op_stack.push(def_op[op]);
+    pushNumber(cur_value);
+    pushOperator(op);
+    const evalRes = optimisticEval();
+    if (evalRes[0]) {
+        showValue(evalRes[1]);
+    }
     cur_value = zero();
-    showValue();
 }
 
 function calculate() {
-    stack.push(cur_value.value);
-    if (stack.length >= 2 && op_stack.length >= 1) {
-        const a = stack.pop();
-        const b = stack.pop();
-        const op = op_stack.pop();
-        const result = op(a,b);
-        stack.push(result);
-        showValue(result);
-    } else {
-        showValue("Missing arguments");
+    pushNumber(cur_value);
+    stack = stack.concat(op_stack);
+    for (var i = 0; i < stack.length - 1; i++) {
+        const val = stack[i];
+        if (val.kind == "num") {
+            stack.push(val);
+        } else {
+            const b = stack.pop().value;
+            const a = stack.pop().value;
+            const fun = val.fun;
+            const res = num_constructor(fun(a,b));
+            stack.push(res);
+        }
     }
-    cur_value = zero();
+    const finalres = stack.pop();
+    stack = [ finalres ];
+    showValue(finalres.value);
 }
 
 // setDecimal();
